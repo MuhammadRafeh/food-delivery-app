3f888d6ce76ebe23d61c7ae23c4bc3810ad4da95
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-08-24 17:14:15 +0500

    Designing and adding fonts

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import AppLoading from 'expo-app-loading';
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { useFonts, Inter_900Black,Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import Constants from 'expo-constants';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     Inter_900Black,
+    Inter_800ExtraBold
   });
 
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontFamily: 'Inter_900Black', fontSize: 40 }}>Inter Black</Text>
+      <View style={styles.screen}>
+        <View style={styles.firstRow}>
+          <View style={styles.circle}/>
+          <View>
+            <Text style={styles.text}>Food For</Text>
+            <Text style={styles.text}>Everyone</Text>
+          </View>
+        </View>
+        <View style={styles.lastRow}>
+          <Button title="Get Started" />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1, 
+    justifyContent: 'space-between',
+    marginTop: Constants.statusBarHeight, 
+    backgroundColor: '#FF4B3A',
+    padding: 42
+  },
+  circle: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: 'white',
+    marginBottom: 12
+  },
+  text: {
+    fontSize: 50,
+    fontFamily: 'Inter_800ExtraBold'
+  },
+  firstRow: {
+
+  },
+  lastRow: {
+
+  }
+})
