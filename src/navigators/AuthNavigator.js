eeddb12a6fbea8798678ba9aea91f83661f578d4
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-08-25 10:02:40 +0500

    Corrected the Layout

--- src/navigators/AuthNavigator.js
+++ src/navigators/AuthNavigator.js
@@ -6,23 +6,27 @@ import Login from '../screens/Auth/Login';
 import Signup from '../screens/Auth/Signup';
 
 const Tab = createMaterialTopTabNavigator();
+const { width } = Dimensions.get('window');
 
 function MyTabs() {
     return (
-        <View style={{flex: 1, marginTop: Constants.statusBarHeight}}>
+        <View style={{ flex: 1, marginTop: Constants.statusBarHeight, backgroundColor: '#F2F2F2' }}>
+            <View style={{ flex: 1, backgroundColor: 'white' }}>
 
-            <Tab.Navigator 
-                initialLayout={{ width: Dimensions.get('window').width }}
-                screenOptions={{
-                    tabBarIndicatorContainerStyle: {justifyContent: 'flex-end', alignItems: 'flex-end' },
-                    tabBarLabelStyle: {fontWeight: 'bold', textTransform: 'none'},
-                    tabBarStyle: {borderBottomLeftRadius: 20, borderBottomRightRadius: 20},
-                    tabBarIndicatorStyle: {width: '35%', backgroundColor: 'rgb(250,74,12)'}
-                }}
-            >
-                <Tab.Screen name="Login" component={Login}/>
-                <Tab.Screen name="Sign-up" component={Signup} />
-            </Tab.Navigator>
+            </View>
+            <View style={{ flex: 2.2 }}>
+                <Tab.Navigator
+                    initialLayout={{ width }}
+                    screenOptions={{
+                        tabBarLabelStyle: { fontWeight: 'bold', textTransform: 'none' },
+                        tabBarStyle: { borderBottomLeftRadius: 20, borderBottomRightRadius: 20 },
+                        tabBarIndicatorStyle: { width: width/3, backgroundColor: 'rgb(250,74,12)', left: width/12 }
+                    }}
+                >
+                    <Tab.Screen name="Login" component={Login} />
+                    <Tab.Screen name="Sign-up" component={Signup} />
+                </Tab.Navigator>
+            </View>
         </View>
 
     );
