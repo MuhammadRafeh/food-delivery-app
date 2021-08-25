10500911633bc1a80283491810f4edda9d33317f
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-08-25 10:36:42 +0500

    Designing Done with Login Screen

--- src/screens/Auth/Login.js
+++ src/screens/Auth/Login.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+const { width } = Dimensions.get('window');
 
 const Login = props => {
     return (
-        <View style={{backgroundColor: '#F2F2F2', flex: 1}}>
-            <Text>
-                Login
-            </Text>
+        <View style={styles.screen}>
+            <View style={styles.firstRow}>
+                <View style={{ flex: 1 }}>
+                    <Text style={styles.inputLableColor}>
+                        Email address
+                    </Text>
+                    <TextInput onChangeText={(val) => { }} style={{ width: '100%', borderBottomColor: 'rgb(121,121,121)', borderBottomWidth: 1 }} />
+                </View>
+                <View style={{ flex: 2.3 }}>
+                    <Text style={styles.inputLableColor}>
+                        Password
+                    </Text>
+                    <TextInput onChangeText={(val) => { }} style={{ width: '100%', borderBottomColor: 'rgb(121,121,121)', borderBottomWidth: 1 }} />
+                    <View style={{marginTop: 29}}>
+
+                        <Text style={{ color: 'rgb(250,74,12)', fontWeight: 'bold', fontSize: 15 }}>
+                            Forgot Passcode?
+                        </Text>
+                    </View>
+                </View>
+                {/* <View style={{flex: 1}}>
+                </View> */}
+            </View>
+            <View style={styles.lastRow}>
+                <TouchableOpacity style={styles.button} >
+                    <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 18 }}>
+                        Login
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -15,7 +42,30 @@ export default Login;
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1
+        flex: 1,
+        backgroundColor: 'rgb(242,242,242)',
+        justifyContent: 'space-between'
+    },
+    firstRow: {
+        flex: 3.5,
+        paddingHorizontal: width / 12,
+        paddingTop: 50,
+        justifyContent: 'flex-start'
+    },
+    button: {
+        backgroundColor: 'rgb(250,74,12)',
+        height: 65,
+        borderRadius: 29,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    lastRow: {
+        flex: 1,
+        paddingHorizontal: width / 12
+    },
+    inputLableColor: {
+        color: 'rgb(145,145,145)',
+        marginBottom: 10
     }
 });
 
--- src/screens/Auth/Signup.js
+++ src/screens/Auth/Signup.js
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const Signup = props => {
     return (
-        <View style={{backgroundColor: '#F2F2F2', flex: 1}}>
+        <View style={styles.screen}>
             <Text>
                 SignUp
             </Text>
@@ -15,7 +15,8 @@ export default Signup;
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1
+        flex: 1,
+        backgroundColor: 'rgb(242,242,242)'
     }
 });
 
