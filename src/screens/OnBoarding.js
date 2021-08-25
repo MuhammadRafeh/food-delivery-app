21efaea2c5acda3061debb3ca5ee1c08ea4dd901
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-08-25 07:48:15 +0500

    Adjusted the Screen!

--- src/screens/OnBoarding.js
+++ src/screens/OnBoarding.js
@@ -8,69 +8,68 @@ import { LinearGradient } from 'expo-linear-gradient';
 const { width } = Dimensions.get('window');
 
 export default function OnBoarding() {
-  let [fontsLoaded] = useFonts({
-    Inter_900Black,
-    Inter_800ExtraBold
-  });
+    let [fontsLoaded] = useFonts({
+        Inter_900Black,
+        Inter_800ExtraBold
+    });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <View style={styles.screen}>
-        <View style={styles.firstRow}>
-          <View style={styles.circle} />
-          <View>
-            <View>
-              <Text style={styles.text}>Food For</Text>
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            <View style={styles.screen}>
+                <View style={styles.firstRow}>
+                    <View style={styles.circle} />
+                    <View>
+                        <View>
+                            <Text style={styles.text}>Food For</Text>
+                        </View>
+                        <View style={{ top: -25 }}>
+                            <Text style={styles.text}>Everyone</Text>
+                        </View>
+                    </View>
+                </View>
+                <LinearGradient
+                    // Background Linear Gradient
+                    colors={['rgb(255,76,59)', 'rgb(255,72,18)', 'rgb(255,74,46)']}
+                    style={{ position: 'absolute', width, height: 300, alignItems: 'center', bottom: 57, left: 0, right: 0 }}
+                />
+                <View style={styles.lastRow}>
+                    <TouchableOpacity style={{ backgroundColor: 'white', height: 50, borderRadius: 20, justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ color: 'rgb(255,75,58)' }}>
+                            Get Started
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={{ top: -25 }}>
-              <Text style={styles.text}>Everyone</Text>
-            </View>
-          </View>
-        </View>
-        <View style={styles.lastRow}>
-            <LinearGradient
-              // Background Linear Gradient
-              colors={['rgb(255,76,59)', 'rgb(255,72,18)', 'rgb(255,74,46)']}//''rgb(255,74,46)'
-              style={{ width, height: 300, alignItems: 'center', bottom: -45, left: -40 }}
-            />
-          {/* <Button title="Get Started" /> */}
-          <TouchableOpacity style={{ backgroundColor: 'white', height: 50, borderRadius: 20, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ color: 'rgb(255,75,58)' }}>
-              Get Started
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'space-between',
-    marginTop: Constants.statusBarHeight,
-    backgroundColor: 'rgb(255,75,58)',
-    padding: 42
-  },
-  circle: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    backgroundColor: 'white',
-    marginBottom: 12
-  },
-  text: {
-    fontSize: 60,
-    fontFamily: 'Inter_800ExtraBold',
-    color: 'white'
-  },
-  firstRow: {
+    screen: {
+        flex: 1,
+        justifyContent: 'space-between',
+        marginTop: Constants.statusBarHeight,
+        backgroundColor: 'rgb(255,75,58)',
+        padding: 42
+    },
+    circle: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        backgroundColor: 'white',
+        marginBottom: 12
+    },
+    text: {
+        fontSize: 60,
+        fontFamily: 'Inter_800ExtraBold',
+        color: 'white'
+    },
+    firstRow: {
 
-  },
-  lastRow: {
+    },
+    lastRow: {
 
-  }
+    }
 })
