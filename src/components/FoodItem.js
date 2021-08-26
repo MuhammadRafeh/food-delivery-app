017f7e8730c9858b937dfe52c6bd4597e63269fc
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-08-26 19:52:39 +0500

    Modified Shadow around Card

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         marginRight: 20,
         marginVertical: 20,
-        elevation: 1,
+        elevation: 5,
         borderRadius: 20,
     }
 });
