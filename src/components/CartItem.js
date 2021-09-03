599de83eb35aec00a84897f1ae32cc7873c0c57d
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-09-03 14:55:13 +0500

    Added Animations to card items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import theme from '../constants/theme';
-import Animated, {useAnimatedGestureHandler, useAnimatedStyle, useSharedValue} from 'react-native-reanimated';
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 import { PanGestureHandler } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 const CartItem = () => {
 
@@ -13,59 +14,79 @@ const CartItem = () => {
             ctx.translateX = swipe.value
         },
         onActive: (e, ctx) => {
-            if (ctx.translateX + e.translationX > 0) {
+            const currentDrag = ctx.translateX + e.translationX
+            if (currentDrag > 0) {
                 swipe.value = 0
                 return
-            }
-            else if (ctx.translateX + e.translationX > -100){
-                swipe.value =  ctx.translateX + e.translationX
+            } else if (currentDrag > -100) {
+                swipe.value = currentDrag
+            } else if (e.velocityX > -100) { //Here it's mean we are placing item back to its position
+                // swipe.value = withTiming(0)
+                swipe.value = withSpring(0, {velocity: e.velocityX})
+
             } else {
-                swipe.value = -100
+                swipe.value = withSpring(-100, {velocity: e.velocityX})
                 return
+            } 
+
+        },
+        onFinish: (e) => {
+            console.log(e.velocityX)
+            if (e.velocityX < -100){
+                swipe.value = withSpring(-100, {velocity: e.velocityX })
             }
-            
         }
     })
 
     const style = useAnimatedStyle(() => {
         return {
             transform: [
-                {translateX: swipe.value}
+                { translateX: swipe.value }
             ]
         }
     })
 
     return (
-        <PanGestureHandler onGestureEvent={handleGestureEvent}>
-            <Animated.View style={[styles.item, style]}>
-                <View style={{ flex: 1 }}>
+        <View>
+            <View style={{ position: 'absolute', bottom: 33, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', right: 20, height: 35 }}>
+                <View style={{backgroundColor: 'rgb(223,44,44)', height: 25}}>
 
+                    <Text>
+                        <Ionicons name={'ios-heart-outline'} size={24} color={'white'}/>
+                    </Text>
                 </View>
-                <View style={{ flex: 3, paddingRight: 10, paddingLeft: 5, justifyContent: 'center' }}>
-                    <View style={{ marginBottom: 6 }}>
-                        <Text numberOfLines={1} style={{ fontSize: 15.5, fontWeight: 'bold', letterSpacing: -1 }}>
-                            Veggie tomato mix asd asd a d a d asd a d s
-                        </Text>
+            </View>
+            <PanGestureHandler onGestureEvent={handleGestureEvent}>
+                <Animated.View style={[styles.item, style]}>
+                    <View style={{ flex: 1 }}>
+
                     </View>
-                    <View>
-                        <Text style={{ color: theme.primary, fontWeight: 'bold', letterSpacing: -0.9 }}>
-                            #1,900
-                        </Text>
+                    <View style={{ flex: 3, paddingRight: 10, paddingLeft: 5, justifyContent: 'center' }}>
+                        <View style={{ marginBottom: 6 }}>
+                            <Text numberOfLines={1} style={{ fontSize: 15.5, fontWeight: 'bold', letterSpacing: -1 }}>
+                                Veggie tomato mix asd asd a d a d asd a d s
+                            </Text>
+                        </View>
+                        <View>
+                            <Text style={{ color: theme.primary, fontWeight: 'bold', letterSpacing: -0.9 }}>
+                                #1,900
+                            </Text>
+                        </View>
+                        <View style={styles.counter}>
+                            <Text style={{ color: 'white', paddingHorizontal: 8 }}>
+                                -
+                            </Text>
+                            <Text style={{ color: 'white' }}>
+                                1
+                            </Text>
+                            <Text style={{ color: 'white', paddingHorizontal: 8 }}>
+                                +
+                            </Text>
+                        </View>
                     </View>
-                    <View style={styles.counter}>
-                        <Text style={{ color: 'white', paddingHorizontal: 8 }}>
-                            -
-                        </Text>
-                        <Text style={{ color: 'white' }}>
-                            1
-                        </Text>
-                        <Text style={{ color: 'white', paddingHorizontal: 8 }}>
-                            +
-                        </Text>
-                    </View>
-                </View>
-            </Animated.View>
-        </PanGestureHandler>
+                </Animated.View>
+            </PanGestureHandler>
+        </View>
     );
 }
 
